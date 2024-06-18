cdc299aae86a4eba593a791506d336c581427504
Rubydog <hirabizz@gmail.com>
2024-06-18 19:07:47 +0900

    fixscript

--- flutter_mobile_app/scripts/melos/update_minor_version.sh
+++ flutter_mobile_app/scripts/melos/update_minor_version.sh
@@ -4,13 +4,13 @@ melos version \
   --no-changelog \
   --no-git-tag-version \
   --no-git-commit-version \
-  --manual-version=mobile_app:minor \
-  --manual-version=feature_app_update:minor \
-  --manual-version=feature_auth:minor \
-  --manual-version=feature_debug_log:minor \
-  --manual-version=feature_flavor:minor \
-  --manual-version=feature_market_analytics:minor \
-  --manual-version=feature_router:minor \
-  --manual-version=feature_theme:minor \
-  --manual-version=feature_todo:minor \
-  --manual-version=feature_user:minor \
+  --manual-version=mobile_app:major \
+  --manual-version=feature_app_update:major \
+  --manual-version=feature_auth:major \
+  --manual-version=feature_debug_log:major \
+  --manual-version=feature_flavor:major \
+  --manual-version=feature_market_analytics:major \
+  --manual-version=feature_router:major \
+  --manual-version=feature_theme:major \
+  --manual-version=feature_todo:major \
+  --manual-version=feature_user:major \
